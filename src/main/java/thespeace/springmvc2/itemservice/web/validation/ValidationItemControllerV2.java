1bce32257be853253992f26fe257ae5be4bf88ec
thespeace <thespeace1@gmail.com>
2024-03-20 15:15:53 +0900

    FEAT: FieldError, ObjectError(V2)

--- src/main/java/thespeace/springmvc2/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/thespeace/springmvc2/itemservice/web/validation/ValidationItemControllerV2.java
@@ -102,7 +102,7 @@ public class ValidationItemControllerV2 {
      *     </li>
      * </ul>
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //검증 로직
@@ -121,6 +121,60 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if(resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item","가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) {
+            log.info("errors = {} ", bindingResult);
+            return "validation/v2/addForm"; //BindingResult는 자동으로 model에 담긴다.
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    /**
+     * <h2>FieldError, ObjectError</h2>
+     * <ul>-FieldError 생성자
+     *     <li>{@code public FieldError(String objectName, String field, String defaultMessage);}</li>
+     *     <li>{@code public FieldError(String objectName, String field, @Nullable Object
+     *         rejectedValue, boolean bindingFailure, @Nullable String[] codes, @Nullable
+     *         Object[] arguments, @Nullable String defaultMessage)}</li>
+     *     <ul>
+     *         <li>objectName : 오류가 발생한 객체 이름</li>
+     *         <li>field : 오류 필드</li>
+     *         <li>rejectedValue : 사용자가 입력한 값(거절된 값)을 저장하는 필드</li>
+     *         <li>bindingFailure : 타입 오류 같은 바인딩 실패인지, 검증 실패인지 구분 값</li>
+     *         <li>codes : 메시지 코드</li>
+     *         <li>arguments : 메시지에서 사용하는 인자</li>
+     *         <li>defaultMessage : 기본 오류 메시지</li>
+     *     </ul>
+     * </ul>
+     */
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        //검증 로직
+        if(!StringUtils.hasText(item.getItemName())) {
+            //'FieldError'는 오류 발생시 사용자 입력 값 유지해주는 기능을 제공한다.
+            bindingResult.addError(new FieldError("item","itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item","price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+        if(item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.addError(new FieldError("item","quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+        }
+
+        //특정 필드의 범위를 넘어서는 복합 룰 검증 로직
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
