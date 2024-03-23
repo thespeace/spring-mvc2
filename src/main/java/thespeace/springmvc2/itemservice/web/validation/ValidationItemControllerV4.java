5e6b095f96b0cfb27f395f9c8d74a833c7e80e0e
thespeace <thespeace1@gmail.com>
2024-03-23 10:01:20 +0900

    REFACTOR: Form transmission object separation(registration,modification + validation)

--- src/main/java/thespeace/springmvc2/itemservice/web/validation/ValidationItemControllerV4.java
+++ src/main/java/thespeace/springmvc2/itemservice/web/validation/ValidationItemControllerV4.java
@@ -12,6 +12,8 @@ import thespeace.springmvc2.itemservice.domain.item.Item;
 import thespeace.springmvc2.itemservice.domain.item.ItemRepository;
 import thespeace.springmvc2.itemservice.domain.item.SaveCheck;
 import thespeace.springmvc2.itemservice.domain.item.UpdateCheck;
+import thespeace.springmvc2.itemservice.web.validation.form.ItemSaveForm;
+import thespeace.springmvc2.itemservice.web.validation.form.ItemUpdateForm;
 
 import java.util.List;
 
@@ -43,12 +45,17 @@ public class ValidationItemControllerV4 {
         return "validation/v4/addForm";
     }
 
+    /**
+     * {@code @ModelAttribute("item")} : item이라고 명시해주지 않으면 규칙에 의해
+     * ItemSaveForm이라는 이름으로 MVC Model에 감기게 된다. 이렇게 되면 뷰 템플릿에서
+     * 접근하는 th:object이름도 함께 변경해주어야 한다.
+     */
     @PostMapping("/add")
-    public String addItem(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //특정 필드 예외가 아닌 전체 예외(글로벌 오류 처리)
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
@@ -60,6 +67,12 @@ public class ValidationItemControllerV4 {
         }
 
         //성공 로직
+        //폼 객체의 데이터를 기반으로 Item 객체를 생성, 폼 객체 처럼 중간에 다른 객체가 추가되면 변환하는 과정이 추가된다.
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -74,11 +87,11 @@ public class ValidationItemControllerV4 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable("itemId") Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+    public String edit(@PathVariable("itemId") Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form, BindingResult bindingResult) {
 
         //특정 필드 예외가 아닌 전체 예외(글로벌 오류 처리)
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
@@ -89,7 +102,12 @@ public class ValidationItemControllerV4 {
             return "validation/v4/editForm";
         }
 
-        itemRepository.update(itemId, item);
+        Item itemParam = new Item();
+        itemParam.setItemName(form.getItemName());
+        itemParam.setPrice(form.getPrice());
+        itemParam.setQuantity(form.getQuantity());
+
+        itemRepository.update(itemId, itemParam);
         return "redirect:/validation/v4/items/{itemId}";
     }
 
