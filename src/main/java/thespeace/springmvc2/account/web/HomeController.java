6d7720980b7770f50181df004c4891111821661d
thespeace <thespeace1@gmail.com>
2024-03-24 17:07:25 +0900

    FEAT: Handling login, logout - use of cookies

--- src/main/java/thespeace/springmvc2/account/web/HomeController.java
+++ src/main/java/thespeace/springmvc2/account/web/HomeController.java
@@ -1,17 +1,41 @@
 package thespeace.springmvc2.account.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import thespeace.springmvc2.account.domain.member.Member;
+import thespeace.springmvc2.account.domain.member.MemberRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/account")
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping
+    private final MemberRepository memberRepository;
+
+//    @GetMapping
     public String home() {
         return "/account/home";
+    }
+
+    @GetMapping
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if(memberId == null) {
+            return "/account/home";
+        }
+
+        //로그인 시
+        Member loginMember = memberRepository.findById(memberId);
+        if(loginMember == null) {
+            return "/account/home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "/account/login/loginHome";
     }
 }
--- src/main/java/thespeace/springmvc2/account/web/login/LoginController.java
+++ src/main/java/thespeace/springmvc2/account/web/login/LoginController.java
@@ -1,5 +1,7 @@
 package thespeace.springmvc2.account.web.login;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +26,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if(bindingResult.hasErrors()) {
             return "account/login/loginForm";
         }
@@ -36,10 +38,33 @@ public class LoginController {
             return "account/login/loginForm";
         }
 
-        //로그인 성공 처리 TODO
+        //로그인 성공 처리
+
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/account";
     }
 
+    /**
+     * <h2>로그아웃</h2>
+     * <ul>
+     *     <li>세션 쿠키이므로 웹 브라우저 종료시</li>
+     *     <li>서버에서 해당 쿠키의 종료 날짜를 0으로 지정</li>
+     * </ul>
+     * 로그아웃도 응답 쿠키를 생성하는데 `Max-Age=0` 를 확인할 수 있다. 해당 쿠키는 즉시 종료된다.
+     */
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/account";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
 
 }
