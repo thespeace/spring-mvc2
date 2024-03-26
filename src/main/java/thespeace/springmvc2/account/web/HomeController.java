a68fa623116f617daa0df5b0ad06cd010c1be9a9
thespeace <thespeace1@gmail.com>
2024-03-26 16:52:22 +0900

    FEAT: Handling login - Servlet HTTP Session 2

--- src/main/java/thespeace/springmvc2/account/web/HomeController.java
+++ src/main/java/thespeace/springmvc2/account/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import thespeace.springmvc2.account.domain.member.Member;
 import thespeace.springmvc2.account.domain.member.MemberRepository;
 import thespeace.springmvc2.account.web.session.SessionManager;
@@ -58,7 +59,7 @@ public class HomeController {
         return "/account/login/loginHome";
     }
 
-    @GetMapping
+//    @GetMapping
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false); //세션을 메모리를 사용하기때문에 꼭 필요할때만 사용.
@@ -67,6 +68,24 @@ public class HomeController {
         }
 
         Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember == null) {
+            return "/account/home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "/account/login/loginHome";
+    }
+
+    /**
+     * <h2>@SessionAttribute</h2>
+     * 이미 로그인 된 사용자를 찾을 때 사용된다. 참고로 이 기능은 세션을 생성하지 않는다.
+     */
+    @GetMapping
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
         //세션에 회원 데이터가 없으면 home
         if(loginMember == null) {
