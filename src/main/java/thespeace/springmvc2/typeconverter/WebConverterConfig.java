b9a3c4f71a821ef64ee99fd31cdad9ddad40a424
thespeace <thespeace1@gmail.com>
2024-04-05 16:11:59 +0900

    FEAT: Formatter - Apply to Web Application

--- src/main/java/thespeace/springmvc2/typeconverter/WebConverterConfig.java
+++ src/main/java/thespeace/springmvc2/typeconverter/WebConverterConfig.java
@@ -7,6 +7,7 @@ import thespeace.springmvc2.typeconverter.converter.IntegerToStringConverter;
 import thespeace.springmvc2.typeconverter.converter.IpPortToStringConverter;
 import thespeace.springmvc2.typeconverter.converter.StringToIntegerConverter;
 import thespeace.springmvc2.typeconverter.converter.StringToIpPortConverter;
+import thespeace.springmvc2.typeconverter.formatter.MyNumberFormatter;
 
 /**
  * <h1>스프링에 Converter 적용</h1>
@@ -19,12 +20,21 @@ public class WebConverterConfig implements WebMvcConfigurer {
 
     /**
      * <h2>컨버터 등록</h2>
+     * <p>
+     * <h2>포멧터 등록</h2>
+     * <a href="http://localhost:8080/converter/converter-view">MyNumberFormatter 적용 확인 test url</a>
+     * <a href="http://localhost:8080/converter/intro/v2?data=10,000">포맷팅 된 문자 -> Integer 타입 변환 적용 확인 test url</a>
      */
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+
+        //우선순위(converter > formatter)로 인해 주석처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
